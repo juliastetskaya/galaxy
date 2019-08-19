8dd41b0d1e1c5f1ce8b2d4a031dbaf23388397d1
juliastetskaya <juliastetskaya@gmail.com>
2019-08-19 18:29:21 +0300

    create SwapiServive for getting data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import SwapiService from './services/swapi-service';
 
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((people) => {
+  people.forEach((p) => console.log(p.name));
+});
