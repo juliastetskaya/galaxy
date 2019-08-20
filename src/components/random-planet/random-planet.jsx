2d73b1fd1eba5c9d65987482b5a3becc6fb73362
juliastetskaya <juliastetskaya@gmail.com>
2019-08-20 07:51:46 +0300

    implement data loading spinner

--- src/components/random-planet/random-planet.jsx
+++ src/components/random-planet/random-planet.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+import PlanetView from './planet-view';
 
 export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
@@ -11,50 +13,28 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
+    loading: true,
   }
 
   onPlanetLoaded = (planet) => {
-    this.setState({ planet });
+    this.setState({ planet, loading: false });
   }
 
   updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
+    const id = Math.floor(Math.random() * 16) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded);
   }
 
   render() {
-    const {
-      planet: {
-        id, name, population, rotationPeriod, diameter,
-      },
-    } = this.state;
+    const { planet, loading } = this.state;
+
+    const content = loading ? <Spinner /> : <PlanetView planet={planet} />;
 
     return (
       <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          alt=""
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        {content}
       </div>
     );
   }
---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div className="lds-css ng-scope spinner">
+    <div className="lds-double-ring">
+      <div />
+      <div />
+    </div>
+  </div>
+);
+
+export default Spinner;
