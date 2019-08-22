f2b5abc4a2962689f668e6d2ec5c3b8e6f9cb27d
juliastetskaya <juliastetskaya@gmail.com>
2019-08-22 21:56:28 +0300

    move error handling in the separate class

--- src/components/people-page/people-page.jsx
+++ src/components/people-page/people-page.jsx
@@ -1,44 +1,43 @@
+/* eslint-disable max-classes-per-file */
 import React, { Component } from 'react';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
-import ErrorIndicator from '../error-indicator';
+import Row from '../row';
+import ErrorBoundary from '../error-boundary';
 import SwapiService from '../../services/swapi-service';
+
 
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
 
   state = {
     selectedPerson: 1,
-    hasError: false,
-  }
-
-  componentDidCatch() {
-    this.setState({ hasError: true });
   }
 
   onItemSelected = (id) => {
     this.setState({ selectedPerson: id });
   }
 
-  render() {
-    const { selectedPerson, hasError } = this.state;
 
-    if (hasError) {
-      return <ErrorIndicator />;
-    }
+  render() {
+    const { selectedPerson } = this.state;
+
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onItemSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({ name }) => name}
+      />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={selectedPerson} loading={false} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onItemSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({ name, birthYear, gender }) => `${name} (${gender}, ${birthYear})`}
-          />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={selectedPerson} loading={false} />
-        </div>
-      </div>
+      <ErrorBoundary>
+        <Row left={itemList} right={personDetails} />
+      </ErrorBoundary>
     );
   }
 }
